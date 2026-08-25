cef46debb49b947d8a18b658d497d9e907c7ac50
MrBrownOff <brun.cedric@gmail.com>
2026-08-25 08:24:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tri-Etiquettes/', // 👈 Remplace "nom-de-ton-depot" par le nom exact de ton dépôt GitHub
+  base: '/Commande-Etiquettes/', // 👈 Remplace "nom-de-ton-depot" par le nom exact de ton dépôt GitHub
 });
